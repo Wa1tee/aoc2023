93ee832c2d861ebf4ed06baced70cfd43fe7c804
John Doe <john.doe@domain.com>
2023-12-01 14:34:30 +0200

    Added main file template and created makefile

---
+++ haskell/day1/main.hs
@@ -0,0 +1,14 @@
+-- Main.hs
+
+module Main where
+
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  
+  -- Your program logic goes here
+  
+  -- Print output or perform other actions based on the arguments
+  putStrLn "Hello, Haskell!"
